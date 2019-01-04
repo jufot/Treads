b39d85c738b397463e6b7594890b1354fb57407e
Jeremiah Ufot <jeremiahufot@Jeremiahs-MacBook-Pro.local>
2019-01-04 15:19:17 -0500

    Added git ignore file

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Jeremiah Ufot on 1/4/19.
+//  Copyright © 2019 Jeremiah Ufot. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Jeremiah Ufot on 1/4/19.
+//  Copyright © 2019 Jeremiah Ufot. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
